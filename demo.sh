627aad9f9a3f9b31ed16925187a5d46ac18e473b
erica <jagg1@126.com>
2017-10-10 00:23:56 +0800

    添加了第5次课：脚本的作业demo.sh文件

---
+++ demo.sh
@@ -0,0 +1,23 @@
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js 
+  touch css/style.css js/main.js index.html
+  echo  "<!DOCTYPE>"                  >  index.html
+  echo  "<title>Hello</title>"       >> index.html
+  echo  "<h1>Hi</h1> "               >> index.html
+  echo  " h1{color: red;}"            > css/style.css
+  echo  "var string = 'Hello World'"  > js/main.js
+  echo  "alert(string)"              >> js/main.js
+  exit
+fi
+
+
+
+
+
+
+
